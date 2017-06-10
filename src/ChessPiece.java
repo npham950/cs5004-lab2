5b3ad37aa6acb9e84512a177084f531e998d0aef
Ny Pham <hongny26@gmail.com>
2017-06-10 00:20:32 -0700

    completed interface

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -2,5 +2,8 @@
  * Created by Steven on 6/10/17.
  */
 public interface ChessPiece {
-
+  int getRow();
+  int getColumn();
+  boolean canMove(int row, int column);
+  boolean canKill(ChessPiece piece);
 }
