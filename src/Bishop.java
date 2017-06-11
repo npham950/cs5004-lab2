d7ba18b6dc6b6f804da7d7350d32f3b86a72b8e4
Ny Pham <hongny26@gmail.com>
2017-06-10 17:32:44 -0700

    update canMove method

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,6 +1,46 @@
 /**
  * Created by Steven on 6/10/17.
  */
-public class Bishop {
+public class Bishop implements ChessPiece {
+  private int row;
+  private int column;
+  private Color color;
 
+  public Bishop(int row, int column, Color color) {
+    verifyRowandColumn(row, column);
+    this.row = row;
+    this.column = column;
+    this.color = color;
+  }
+
+  @Override
+  public int getRow() {
+    return this.row;
+  }
+
+  @Override
+  public int getColumn() {
+    return this.column;
+  }
+
+  @Override
+  public boolean canMove(int row, int column) {
+    verifyRowandColumn(row, column);
+    return this.row != row && this.column != column && Math.abs(row - this.row) == Math.abs(column - this.column);
+  }
+
+  @Override
+  public boolean canKill(ChessPiece piece) {
+    return false;
+  }
+
+  public boolean isValid(int n) {
+    return (n >= 0 && n <= 7);
+  }
+
+  private void verifyRowandColumn (int row, int column) {
+    if (!isValid(row) || !isValid(column)) {
+      throw new IllegalArgumentException("Non-negative number only");
+    }
+  }
 }
